19ebcb92dcb683be94c6b0e063eae09790aa9ed4
ohhiri <138719445+ohhiri@users.noreply.github.com>
2025-07-14 20:49:30 -0400

    Added createTask

--- ConsoleAppToDo/Program.cs
+++ ConsoleAppToDo/Program.cs
@@ -2,29 +2,46 @@
 
 namespace smallHourProject
 {
+
+  public class Task
+  {
+    public string Name { get; set; }
+    public int Priority { get; set; }
+    public Task(string name, int priority)
+    {
+      Name = name;
+      Priority = priority;
+    }
+  }
+
+
   class Program
   {
     static int userInput;
     static string? input;
+    static DateTime localDate = DateTime.Now;
+    static int count = 0;
+    static private List<Task> _tasks = new List<Task>();
+
     static void PromptUser()
     {
-      Console.WriteLine("Type 1 to add, 2 to remove, 3 to rename, 4 to reprioritize");
+      Console.WriteLine("Type 1 to add, 2 to remove, 3 to list, 4 to rename, 5 to reprioritize");
       input = Console.ReadLine();
       if (int.TryParse(input, out userInput))
       {
         switch (userInput)
         {
           case 1:
-            Console.WriteLine("Test 1");
+            createTask();
             break;
           case 2:
-            Console.WriteLine("Test 2");
+            // deleteTask();
             break;
           case 3:
-            Console.WriteLine("Test 3");
+            // renameTask();
             break;
           case 4:
-            Console.WriteLine("Test 4");
+            // repoTask();
             break;
           default:
             Console.WriteLine("Invalid Input, reprompting");
@@ -39,8 +56,28 @@ namespace smallHourProject
       }
     }
 
+
+
+    static void createTask()
+    {
+      Console.WriteLine("Assign a name to this task");
+      string name = Console.ReadLine();
+      Console.WriteLine("Assign a priority to this task 1 through 5");
+      int priority = Int32.Parse(Console.ReadLine());
+      Task task = new Task(name, priority);
+      _tasks.Add(task);
+    }
+
+    void removeTask(Task task)
+    {
+
+    }
+
+
     static void Main(string[] args)
     {
+      Console.WriteLine("Welcome to my first C# Project :) sorry it sucks lol");
+      Console.WriteLine("Current time is " + localDate);
       PromptUser();
     }
   }
--- ConsoleAppToDo/obj/Debug/net9.0/ConsoleAppToDo.AssemblyInfo.cs
+++ ConsoleAppToDo/obj/Debug/net9.0/ConsoleAppToDo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleAppToDo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c283c1c835b03f7660be521428c363f2934c6c71")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleAppToDo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleAppToDo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
